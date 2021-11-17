b8cd9a2414f0df3faa101bb8c7ed4c93b5956c84
George Lopatenko <lopa10ko239@mail.ru>
2021-11-17 14:24:35 +0300

    refactor: added single test

--- console_calculator.py
+++ console_calculator.py
@@ -1,13 +1,17 @@
-def main():
-    print("Give your expression: ")
-    parsed_str = input().replace(' ', '').replace('--', '+').replace('-', '+-').split('+')
+
+
+def test_main():
+    assert (main(('45 + 4 - 242442 + 32342'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == -210051)
+    print('all checked')
+    # assert main('')
+
+def main(parsed_str):    
     answer = 0
     for i in range(len(parsed_str)):
         answer += int(parsed_str[i])
-    print('Your answer: ', answer)
+    return answer
 if __name__ == '__main__':
-    main()
+    print("Give your expression: ")
+    print(main(parsed_str = input().replace(' ', '').replace('--', '+').replace('-', '+-').split('+')))
+    test_main()
 
-
-# def test_main():
-#     assert main()
