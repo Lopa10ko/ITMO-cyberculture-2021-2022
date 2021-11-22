c0ea86a861cd295e08888f964336cb83bedd9371
George Lopatenko <lopa10ko239@mail.ru>
2021-11-22 10:59:02 +0300

    added unit_testing

--- console_calculator.py
+++ console_calculator.py
@@ -1,9 +1,14 @@
-
-
 def test_main():
     assert (main(('45 + 4 - 242442 + 32342'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == -210051)
+    assert (main(('56-757+5888-583+784+8858+1+1'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == 14248)
+    assert (main(('1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == 45)
+    assert (main(('58838 -544 + 85858 - 29296 + 5930 - 5903 -4444'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == 110439)
+    assert (main(('5773+5885 -4 + 584828 -588 + 58 - 58588'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == 537364)
+    assert (main(('884882 - 4848848 + 2 - 3484'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == -3967448)
+    assert (main(('453--43+4433-43343 +44'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == -38370)
+    assert (main(('437774  + 848484848-         488'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == 848922134)
+    assert (main(('8 88 8 8 8 8 8 -  7 7 77 7 7 '.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))) == 88111111)
     print('all checked')
-    # assert main('')
 
 def main(parsed_str):    
     answer = 0
---
+++ unittesting_console_calculator.py
@@ -0,0 +1,23 @@
+import unittest
+from console_calculator import main
+
+class Testing(unittest.TestCase):
+
+    @unittest.skip("demonstrating skipping")
+    def test_nothing(self):
+        self.fail("shouldn't happen")
+
+    def test_calculator(self):
+        self.assertEqual(main(('45 + 4 - 242442 + 32342'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))), -210051)
+        self.assertEqual(main(('56-757+5888-583+784+8858+1+1'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))), 14248)
+        self.assertEqual(main(('1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))), 45)
+        self.assertEqual(main(('58838 -544 + 85858 - 29296 + 5930 - 5903 -4444'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))), 110439)
+        self.assertEqual(main(('5773+5885 -4 + 584828 -588 + 58 - 58588'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))), 537364)
+        self.assertEqual(main(('884882 - 4848848 + 2 - 3484'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))), -3967448)
+        self.assertEqual(main(('453--43+4433-43343 +44'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))), -38370)
+        self.assertEqual(main(('437774  + 848484848-         488'.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))), 848922134)
+        self.assertEqual(main(('8 88 8 8 8 8 8 -  7 7 77 7 7 '.replace(' ', '').replace('--', '+').replace('-', '+-').split('+'))), 88111111)
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
